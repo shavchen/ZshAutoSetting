0174e85eb423832963894c8e7d7f9f341dfa1a9c
shavchen <34539839+shavchen@users.noreply.github.com>
2023-09-02 19:50:06 +0800

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -3,6 +3,6 @@ curl -fsSL https://raw.githubusercontent.com/shavchen/ZshAutoSetting/main/.zshrc
 curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh | sh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#sed -i 's/plugins=(git)/plugins=(git  zsh-autosuggestions zsh-syntax-highlighting )/' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git  zsh-autosuggestions zsh-syntax-highlighting )/' ~/.zshrc
 source ~/.zshrc
 zsh
